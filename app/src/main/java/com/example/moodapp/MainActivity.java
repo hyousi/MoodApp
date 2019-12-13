63623dc428ac2e4db1badf032007969f394ad3e3
Ze Dang <4649tz@gmail.com>
2019-12-13 21:28:13 +0800

    fix: bug - app bar appears on loading page. Now, app bar only appears after auth.

--- app/src/main/java/com/example/moodapp/MainActivity.java
+++ app/src/main/java/com/example/moodapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.moodapp;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,19 +26,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.homeFragment).build();
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
 
-        // FIXME: should launch the app bar and drawer after login.
-//        drawerLayout = findViewById(R.id.drawer_layout);
-
-
-//        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
-
-
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller,
+                                             @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.loadingFragment) {
+                    toolbar.setVisibility(View.GONE);
+//                    bottomNavigationView.setVisibility(View.GONE);
+                } else {
+                    toolbar.setVisibility(View.VISIBLE);
+//                    bottomNavigationView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        return NavigationUI.navigateUp(navController, drawerLayout);
-//    }
 }
