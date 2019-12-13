04c31eb3f3916ebb3fb65840c9b4d80368d3bb57
Ze Dang <4649tz@gmail.com>
2019-12-13 22:46:15 +0800

    fix: bottom nav logic.

--- app/src/main/java/com/example/moodapp/MainActivity.java
+++ app/src/main/java/com/example/moodapp/MainActivity.java
@@ -16,9 +16,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private BottomNavigationView bottomNav;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +30,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+        bottomNav = findViewById(R.id.bottom_nav);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(R.id.homeFragment)
+                new AppBarConfiguration.Builder(R.id.homeFragment, R.id.diaryListFragment)
                         .setDrawerLayout(drawerLayout)
                         .build();
-        final Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
+
+        // set up auto nav for app bar.
         NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        // set up auto nav for bottom nav bar.
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
         navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
             @Override
@@ -45,10 +54,18 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.registrationFragment:
                         toolbar.setVisibility(View.GONE);
                         drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+                        bottomNav.setVisibility(View.GONE);
+                        break;
+                    case R.id.homeFragment:
+                    case R.id.diaryListFragment:
+                        toolbar.setVisibility(View.VISIBLE);
+                        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                        bottomNav.setVisibility(View.VISIBLE);
                         break;
                     default:
                         toolbar.setVisibility(View.VISIBLE);
                         drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                        bottomNav.setVisibility(View.GONE);
                         break;
                 }
             }
